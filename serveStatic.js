3f125c907a5c6f8431a3eb973bee1eb6fc280132
asnicode <asneeboy2@gmail.com>
2025-08-17 22:41:15 -0400

    new

---
+++ serveStatic.js
@@ -0,0 +1,25 @@
+import path from 'node:path'
+import { fs } from 'node:fs/promises'
+import { sendResponse } from './sendResponse'
+import { getContentType } from './mime.js'
+export async function serveStatic(req, res, base) {
+    const filepath = path.join(base, req.url === '/' ? 'index.html' : req.url)
+    const ext = path.extname(filepath)
+    const contentType = getContentType(ext)
+    try {
+        const content = await fs.readFile(filepath)
+        sendResponse = sendResponse(res, 200, contentType, content)
+
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            const content = await fs.readFile('./404.html', 'utf-8')
+            sendResponse(res, 404, contentType, content)
+        } else {
+            sendResponse(res, 500, 'txt/html', "<html><p>error code:${err.code}</p></html>")
+        }
+
+    }
+
+
+
+}
